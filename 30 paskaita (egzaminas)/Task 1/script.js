bee771b9a208affe2a6a6337c6a1eac9be495863
smoking-kills-airbases <gelzbetonis369@gmail.com>
2022-10-07 20:45:02 +0300

    Update script.js

--- 30 paskaita (egzaminas)/Task 1/script.js
+++ 30 paskaita (egzaminas)/Task 1/script.js
@@ -8,3 +8,14 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+let inputField = document.getElementById("search")
+let submitInputButton = document.getElementById("submit-btn")
+
+let kilos = inputField.value
+let poundConvertion = kilos * 2.2046
+let gramConvertion = kilos * 0.0010000
+let ounceConvertion = kilos * 35.274
+
+console.log(inputField);
+console.log(submitInputButton);
+
