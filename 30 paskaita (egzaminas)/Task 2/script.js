41e94554409e71b92174026361dd1c563bd2e88f
smoking-kills-airbases <gelzbetonis369@gmail.com>
2022-10-08 13:01:04 +0300

    Update script.js

--- 30 paskaita (egzaminas)/Task 2/script.js
+++ 30 paskaita (egzaminas)/Task 2/script.js
@@ -3,3 +3,15 @@ Parašykite JS kodą, kuris skaičiuos kiek kartų buvo paspaustas mygtukas
 su tekstu "CLICK ME". Paspaudimų rezultatas turi būti matomas dešinėje
 pusėje esančiame "state" skaičiavimo bloke (<div id="btn__state">0</div>)
 ------------------------------------------------------------------- */
+
+let clickeroo = document.getElementById("btn__element")
+let counteroo = document.getElementById("btn__state")
+let clickCount = counteroo.innerText
+
+// console.log(counteroo.innerText);
+
+clickeroo.addEventListener("click", function(){
+    let result = Number(counteroo.innerText) + 1;
+
+        counteroo.innerText = result
+})
