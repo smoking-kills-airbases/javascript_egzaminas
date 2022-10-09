8765624fded1c3a80ca586eda5567df1c9306a86
smoking-kills-airbases <gelzbetonis369@gmail.com>
2022-10-09 15:56:11 +0300

    Update script.js

--- 30 paskaita (egzaminas)/Task 9/script.js
+++ 30 paskaita (egzaminas)/Task 9/script.js
@@ -6,3 +6,21 @@ title, director, budget
 Metodas: 
 wasExpensive() - jeigu filmo budget bus didesnę nei 100 000 000 mln USD, tada gražins true, kiru atveju false 
 ------------------------------------------------------------------------------------------------------ */
+
+
+class Movie {
+    constructor(title, director, budget) {
+        this.title = title;
+        this.director = director;
+        this.budget = budget;
+    }
+   wasExpensive() {
+    if (this.budget >= 100000000) {
+        console.log('true');
+    } else {console.log('false');}
+   }
+}
+
+let blockbuster = new Movie('Sprogimai 36 sugryzimas', 'Antanas Smetona', 120000000)
+
+console.log(blockbuster.wasExpensive())
