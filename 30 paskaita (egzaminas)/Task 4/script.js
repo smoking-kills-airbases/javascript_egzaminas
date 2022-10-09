dc935a73a252b1b19dba367e5f22ca723a817c7e
smoking-kills-airbases <gelzbetonis369@gmail.com>
2022-10-09 12:14:21 +0300

    Update script.js

--- 30 paskaita (egzaminas)/Task 4/script.js
+++ 30 paskaita (egzaminas)/Task 4/script.js
@@ -10,3 +10,15 @@ bent minimalų stilių;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'cars.json';
+
+fetch(ENDPOINT)
+    .then(res => res.json())
+    .then(
+        data => {
+
+            for (let i=0; i=data.length; i++){
+                console.log(data[i])
+            }
+
+        }
+    )
