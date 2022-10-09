d786858a535a0e2c428e759d58c6353069ffb51c
smoking-kills-airbases <gelzbetonis369@gmail.com>
2022-10-09 15:45:13 +0300

    Update script.js

--- 30 paskaita (egzaminas)/Task 8/script.js
+++ 30 paskaita (egzaminas)/Task 8/script.js
@@ -5,3 +5,28 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą.
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+
+function Calculator(a, b) {
+    this.a = a;
+    this.b = b;
+    this.sum = function () {
+        return a+b
+    };
+    this.subtraction = function () {
+        return a-b
+    };
+    this.multiplication = function () {
+        return a*b
+    };
+    this.division = function () {
+        return a/b
+    };
+}
+
+let numbers = new Calculator(9, 6)
+console.log(numbers.sum())
+console.log(numbers.subtraction())
+console.log(numbers.multiplication())
+console.log(numbers.division())
+
